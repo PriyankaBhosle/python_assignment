dd608dff45474f57bc5bb15fa4f9d16f4c7243d8
PriyankaBhosle <127030203+PriyankaBhosle@users.noreply.github.com>
2023-03-05 21:22:06 +0530

    Add files via upload

---
+++ assignment_que1.py
@@ -0,0 +1,18 @@
+#write a program to get the fibonacci series between 0 to 50
+
+#fibonacci series 0 to 50
+
+num1 = 0
+num2 = 1
+
+for i in range(50):
+    print(num1)
+    next_num = num1 + num2 
+    num1 = num2
+    num2 =next_num
+
+
+
+
+
+
