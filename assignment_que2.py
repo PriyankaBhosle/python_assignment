fb8783d28f8cd4b903c9a36b5f737fd39529d97d
PriyankaBhosle <127030203+PriyankaBhosle@users.noreply.github.com>
2023-03-05 22:07:15 +0530

    Add files via upload

---
+++ assignment_que2.py
@@ -0,0 +1,20 @@
+# write a python code that accepts a word from the user and reverse it
+
+#1st code sample code Edyoda
+
+str = "Edyoda"
+output = []
+
+for i in range (-1, -7, -1):
+    output.append (str[i])
+print(output)
+
+
+# 2nd code printed hello everyone
+
+str = ("Hello everyone")
+output = []
+
+for i in range (-1, -15, -1):
+    output.append (str[i])
+print(output)
